a748e37f5fb5df464727c9af8d8a2730e7b103d6
Rob Knopf <rknopf@whirlinggizmo.com>
2024-11-15 14:13:15 -0500

    tweak to show hotreload in action

--- scripts/Test.hx
+++ scripts/Test.hx
@@ -8,7 +8,7 @@ class Test extends Script {
 	function onLoad() {
 		log("Test.load");
 		log(ctx); // testing context:  first time should have "HELLO" (set by ScriptInstance), second should have 'mytest' and 'foo' (set by onUnload)
-		log("HERE ONE MORE TIME");
+		log("CHANGE ME TO SEE HOTRELOAD (must be running vite and have enable-script-reload + debug in main.hxml)");
 		event.on("attack", function(data) {
 			log('Attack received with bar: ' + Std.int(data.damage));
 		});
